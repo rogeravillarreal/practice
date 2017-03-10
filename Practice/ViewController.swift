3bdc5d7fe5d94dbdba5fd0964d289b67cd1f6f8f
My first commit <rogervillarreal@Rogers-MacBook.local>
2017-03-10 10:52:07 -0600

    My first commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Roger Villarreal on 3/9/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var textLabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func pushedButton(_ sender: UIButton) {
+        textLabel.text = "Hello Roger!"
+        tapCount += 1
+        
+        if tapCount >= 10 {
+            textLabel.text = "You tapped the button \(tapCount) times!"
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
